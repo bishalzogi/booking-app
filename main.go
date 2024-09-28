ee3ab57d0be294ab7542397e476f0e1f68e11e6c
BishalZogi <yogibishal19@gmail.com>
2024-09-28 14:31:18 -0400

    variables

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
-packages main
- import "fmt"
+package main
 
- func main()  {
-	fmt.Println("welcome to our conference booking application")
-	fmt.Println("Get your ticket here to attend")
- }
+import "fmt"
+
+func main() {
+	var confrenceName = "Go confrence"
+	const confrenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("confranceTickets is %T, remainingTickets is %T, confrenceName is %T\n", confrenceTickets, remainingTickets, confrenceName)
+
+	fmt.Printf("Welcome to %v booking application\n", confrenceName)
+	fmt.Printf("We have total of %v tickets and %v tickets available\n", confrenceTickets, remainingTickets)
+	fmt.Println("Get your ticket here to attende")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+
+}
